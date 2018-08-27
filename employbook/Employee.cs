f3b08b3da757d3372b081019f2cc2e2c6c62f0f8
Hayat <hayatmk4@gmail.com>
2018-08-26 21:53:19 -0700

    initial commit

---
+++ employbook/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employbook
+{
+    public class Employee
+    {
+        //syntax for property prop tap tap
+        /// <summary>
+        /// this is a coment or summary 
+        /// </summary> 
+        
+        public string Name { get; set; }
+        public string Title { get; set; }   
+        public int PhoneNumbers { get; set; }
+        public static Employee GetEmployee() //static is an instance for all instance to use, oposite of static is instance 
+        {
+            //create an object 
+            var employee = new Employee()
+
+            //employee.Name = " John Doe";
+            //employee.Title = "CEO"; or can object initilize like bellow
+            {
+                Name = "John Doe",
+                Title = "CEO",
+              
+            };
+            return employee;
+        }
+        /// <summary>
+        /// wtire employee to file 
+        /// </summary>
+        /// <param name="employee">the employee object to write</param>
+        public static  void WriteEmployee(Employee employee)
+        {
+            var employeeData = $"{employee.Name},{employee.Title}";
+           FileHelper.WriteTextFileAsync("EmoloyeeTextFile.txt", employeeData);
+        }
+    }
+}
---
+++ employbook/FileHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace employbook
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// write content to the file
+        /// </summary>
+        /// <param name="filename">name of the file</param>
+        /// <param name="content">content to write to the file</param>
+        public static async void WriteTextFileAsync(string filename, string content)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+           var textFile = await localFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+           var textStream= await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+        }
+
+    }
+}
